4dcb85b6ff66a05cff63b38960a8f5675812b36a
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-08-03 15:31:16 -0400

    $1

--- docs/conf.py
+++ docs/conf.py
@@ -9,14 +9,15 @@
 project = 'Peripheral Neuropathy Notes'
 copyright = '2023, Kurt'
 author = 'Kurt'
-release = '.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-html_theme = 'piccolo_theme'
+extensions = ['myst_parser']
 
-extensions = [ 'myst-parser']
+myst_enable_extensions = [
+  "colon_fence", "deflist"
+]
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -31,4 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. Peripheral Neuropathy Notes documentation master file, created by
-   sphinx-quickstart on Thu Aug  3 14:35:24 2023.
+   sphinx-quickstart on Thu Aug  3 15:19:50 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
