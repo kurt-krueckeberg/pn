b445c377de0d69321d23e9efb512150255f1bb61
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-08-03 15:07:50 -0400

    $1

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,15 @@ release = '.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+html_theme = 'piccolo_theme'
+
 extensions = [ 'myst-parser']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +31,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
 html_static_path = ['_static']
